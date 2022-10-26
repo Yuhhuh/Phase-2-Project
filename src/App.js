2462f0b2f3d821685b774b08eb918593d6bac42c
Mikhail <mikhailtwere@gmail.com>
2022-10-25 21:25:02 -0600

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-//import logo from "./logo.svg";
-import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
+import "./styles.css";
 
-function App() {
-  //const [isLoaded, setIsLoaded] = useState(false);
-  const [mainVid, setMainVid] = useState([]);
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-  useEffect(() => {
-    fetch("http://localhost:3000/videos/")
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((data) => {
-        console.log("hi");
-        console.log(data);
-        setMainVid(data.video);
-        //setIsLoaded(true);
-      });
-  }, []);
+// PAGES
+import GalleryHome from "./pages/GalleryHome";
+import ImageMain from "./pages/ImageMain";
 
-  /*function getVideos() {
-    fetch("http://localhost:3000/videos/")
-    .then((response) => response.json)
-    .then((data) => {
-      console.log("Hi")
-      console.log(data)
-      //setVideo(data.video)
-    })
-  }*/
-
-  //if (!isLoaded) return <p>Loading...</p>
-
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <video width="320" height="240" controls>
-          <source src={mainVid} type="video/mp4" />
-        </video>
-        <p></p>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<GalleryHome />} />
+          <Route path="/imageMain" element={<ImageMain />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
