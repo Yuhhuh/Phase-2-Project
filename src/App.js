35c6d8661a6f4f5977ef359bd6443a5957390435
Mikhail Were <mikhailtwere@gmail.com>
2022-12-01 19:32:36 -0700

    App Update
    Added useEffect and Props for distributing across the website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
 // PAGES
 import ImageMain from "./pages/ImageMain";
@@ -13,6 +14,40 @@ import "./pages/css/styles.css";
 import GalleryHome from "./pages/GalleryHome";
 
 export default function App() {
+  const [imageFiles, setImageFiles] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:3000/images");
+    return response.json();
+  };
+
+  useEffect(() => {
+    fetchData().then((response) => {
+      setImageFiles(response);
+    });
+
+    return () => {};
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<GalleryHome imageFiles={imageFiles} />}
+          />
+          <Route path="images/:id" element={<ImageMain />} />
+          <Route path="images/new" element={<ImageAdd setImageFiles={setImageFiles}/>} />
+          <Route path="/AboutPage" element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+/*export default function App(id) {
   const [imageFiles, setImages] = useState([]);
 
   useEffect(() => {
@@ -23,7 +58,7 @@ export default function App() {
       .then((data) => {
         setImages(data);
       });
-  }, []);
+  }, [id]);
 
   return (
     <div className="App">
@@ -32,10 +67,10 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<GalleryHome />} />
           <Route path="images/:id" element={<ImageMain />} />
-          <Route path="/:id" element={<ImageAdd />} />
+          <Route path="images/new" element={<ImageAdd />} />
           <Route path="/AboutPage" element={<AboutPage />} />
         </Routes>
       </Router>
     </div>
   );
-}
+}*/
