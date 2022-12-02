eec341a51767092b13f9270901397a9639ac4016
Mikhail Were <mikhailtwere@gmail.com>
2022-12-02 00:21:29 -0700

    App update
    deleting unnecessary code

--- src/App.js
+++ src/App.js
@@ -39,38 +39,13 @@ export default function App() {
             element={<GalleryHome imageFiles={imageFiles} />}
           />
           <Route path="images/:id" element={<ImageMain />} />
-          <Route path="images/new" element={<ImageAdd setImageFiles={setImageFiles}/>} />
+          <Route
+            path="images/new"
+            element={<ImageAdd setImageFiles={setImageFiles} />}
+          />
           <Route path="/AboutPage" element={<AboutPage />} />
         </Routes>
       </Router>
     </div>
   );
 }
-
-/*export default function App(id) {
-  const [imageFiles, setImages] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/images/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setImages(data);
-      });
-  }, [id]);
-
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<GalleryHome />} />
-          <Route path="images/:id" element={<ImageMain />} />
-          <Route path="images/new" element={<ImageAdd />} />
-          <Route path="/AboutPage" element={<AboutPage />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}*/
